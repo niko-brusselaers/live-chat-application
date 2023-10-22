79487dfc9dc3124d0da8283bdaaa70f795fd74f1
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-10-22 16:40:37 +0200

    refacotor: updated sidenav style
    updated style of sidenav

--- images/frontend/src/core/components/navigation/SideNav.tsx
+++ images/frontend/src/core/components/navigation/SideNav.tsx
@@ -1,14 +1,23 @@
-import { Badge, ListGroup } from "react-bootstrap";
+import style from "./css/SideNav.module.css";
 import { ISideNavChats } from "../../shared/interfaces/sideNavChats";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear,faRotateRight,faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
 
 function SideNav({userChats}:{userChats:ISideNavChats[]}) {
     return (  
-        <ListGroup className="w-25 h-100 p-3" >
+        <div className={style.sideNavContainer} >
+            <div className={style.chatsContainer}>
             {(userChats.map((chat, index) => 
-            <ListGroup.Item action href="#" className=" w-50 mb-3 bg-dark text-white text-center"
-            key={index}>{chat.user} {(chat.newMessages ? <Badge>{chat.newMessages}</Badge>: <></>)}
-            </ListGroup.Item> ))}
-        </ListGroup>
+            <a  href="#" key={index}>{chat.user} {(chat.newMessages ? <span>{chat.newMessages}</span>: <></>)}
+            </a> ))}
+            </div>
+            <div className={style.sideNavButtonsContainer}>
+                <button className={style.sideNavButton}><FontAwesomeIcon icon={faRotateRight} /></button>
+                <button className={style.sideNavButton}><FontAwesomeIcon icon={faGear} /></button>
+                <button className={style.sideNavButton}><FontAwesomeIcon icon={faRightFromBracket} /></button>
+            </div>
+        </div>
     );
 }
 
